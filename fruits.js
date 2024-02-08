ef9c4c09a86f1e93714eb9c547fae0d531ef7c73
nsching <143790749+nsching@users.noreply.github.com>
2024-02-07 18:16:18 -1000

    yes

--- fruits.js
+++ fruits.js
@@ -1,1 +1,8 @@
+const fruits = ['apple', 'banana', 'orange', 'grape', 'kiwi', 'melon'];
+function divideEvenOddLengths(list) {
+  return _.groupBy(list, function (input) {
+    return (input.length % 2) === 0 ? 'even' : 'odd';
+  });
+}
 
+console.log(divideEvenOddLengths(fruits));
